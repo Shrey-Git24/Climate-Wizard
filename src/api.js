5f831af025e94dd19c09918d9d7dd30c2223a326
Shrey <pandeyshrey45@gmail.com>
2024-02-20 00:12:35 +0530

    initial commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "86c281ac5emsh83d0b25c66e4c48p178dbbjsn8e3dd216bc0f",// enter your rapid api key here
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "648ff8de607a3cf314e4196cb3a08abe"; // enter your key from openweather API
